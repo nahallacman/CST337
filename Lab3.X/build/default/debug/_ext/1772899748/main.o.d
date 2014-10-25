c97be165613ea7cf1609eb3ff9993fa14c61b7c0
Cal <nahallacman@gmail.com>
2014-10-25 00:28:53 -0700

    Lab 3 initial

---
+++ Lab3.X/build/default/debug/_ext/1772899748/main.o.d
@@ -0,0 +1,2 @@
+build/default/debug/_ext/1772899748/main.o:  \
+ C:/Users/cal/Desktop/School/CST337/CST337/Lab3.X/main.c 
---
+++ Lab3.X/build/default/production/_ext/1772899748/main.o.d
@@ -0,0 +1,2 @@
+build/default/production/_ext/1772899748/main.o:  \
+ C:/Users/cal/Desktop/School/CST337/CST337/Lab3.X/main.c 
