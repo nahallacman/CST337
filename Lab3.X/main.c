e1949d49cd3e41e14572b157b53d9f5b9c6ac132
Cal <nahallacman@gmail.com>
2014-10-27 03:33:37 -0700

    Some work on lab 3

--- Lab3.X/main.c
+++ Lab3.X/main.c
@@ -34,11 +34,48 @@
 
 
 #include <p32xxxx.h>
+#include <plib.h>
+
+
+//setting the system to 72 MHz operation, with a PBclk of 72 MHz
+#pragma config POSCMOD=XT, FNOSC=PRIPLL, FPLLIDIV=DIV_2, FPLLMUL=MUL_18, FPLLODIV=DIV_1
+#pragma config FPBDIV=DIV_1, FWDTEN=OFF, CP=OFF, BWP=OFF
+#pragma config FSOSCEN=OFF, IESO=OFF
+
+
+
+
+
+
+
 
 //	Function Prototypes
 int main(void);
 
 int main(void) {
+    //set the system to 72MHz operation, 2 wait states, use prefetch cache, use instruction cache, disable bmx breakpoints
+    SYSTEMConfig(72000000L, SYS_CFG_WAIT_STATES | SYS_CFG_PCACHE);
+
+    //disable everything about t2 just in case
+    T2CONbits.ON = 0; // make sure the timer is off just in case
+    mT2ClearIntFlag(); // clear the interupt flag just in case
+
+    //configure timer 2
+    PR2 = 511; //set the period register to interrupt every 512 counts
+    T2CONbits.TCKPS = 0; //set the prescaler to 1:1
+
+    //turn on multi vectored mode
+    INTCONbits.MVEC = 1;
+
+    //set the priority of the service routine
+    #pragma interrupt T2ISR IPL4 vector 8
+    
+    //enable interrupts
+    asm("ei");
+
+    //T2CONbits.ON = 1; // start the timer
+
+
     int a = 0;
     while (1);
     {
