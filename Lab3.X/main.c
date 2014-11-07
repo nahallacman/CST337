b64551d7f7939fd889e8bc597649fa701e3e5364
Cal <nahallacman@gmail.com>
2014-11-07 02:31:49 -0800

    Lab 3 work
    Done until part 16

--- Lab3.X/main.c
+++ Lab3.X/main.c
@@ -46,9 +46,15 @@
 //set the priority of the timer2 service routine
 #pragma interrupt T2ISR IPL4 vector 8
 
+//set the priority of the timer3 service routine
+#pragma interrupt T3ISR IPL3 vector 12
+
 void T2ISR(void);
 
+void T3ISR(void);
+
 int global_count;
+int global_count_2;
 
 //	Function Prototypes
 int main(void);
@@ -61,10 +67,18 @@ int main(void) {
     T2CONbits.ON = 0; // make sure the timer is off just in case
     mT2ClearIntFlag(); // clear the interupt flag just in case
 
+    T3CONbits.ON = 0; // make sure the timer is off just in case
+    mT3ClearIntFlag(); // clear the interupt flag just in case
+
     //configure timer 2
     PR2 = 511; //set the period register to interrupt every 512 counts
     T2CONbits.TCKPS = 0; //set the prescaler to 1:1
     TMR2 = 0x0; // zero out the timer value just in case
+
+    //configure timer 3
+    PR3 = 999; //set the period register to interrupt every 512 counts
+    T3CONbits.TCKPS = 0; //set the prescaler to 1:1
+    TMR3 = 0x0; // zero out the timer value just in case
 
     //turn on multi vectored mode
     INTCONbits.MVEC = 1;
@@ -73,8 +87,15 @@ int main(void) {
     mT2SetIntPriority(4);
     // and sub priority
     mT2SetIntSubPriority(0);
-    //Then enable the interrupt (TODO: double check if this is an atomic operation)
+    //Then enable the interrupt
     mT2IntEnable(1);
+
+    //configure the timer 3 priority
+    mT3SetIntPriority(3);
+    // and sub priority
+    mT3SetIntSubPriority(0);
+    //Then enable the interrupt
+    mT3IntEnable(1);
 
     //enable interrupts
     asm("ei");
@@ -82,11 +103,21 @@ int main(void) {
     //configure the LED for output
     TRISDCLR = 1;
 
-    //initalize global counter
+    //initalize global counters
     global_count = 0;
+    global_count_2 = 0;
 
-    T2CONbits.ON = 1; // start the timer
+    //T2CONbits.ON = 1; // start timer 2
 
+    //T3CONbits.ON = 1; // start timer 3
+
+    //timer sync works by setting the timer 3 timer value to 3 to (exactly)
+    //      account for the instruction offset in starting both timers
+    //timer sync: // DONT ALTER THESE 4 LINES, offset has been tested!
+    TMR2 = 0;
+    TMR3 = 3;
+    T2CONSET = 0x8000;
+    T3CONSET = 0x8000;
 
     int a = 0;
     int MAX_NUM = 7200000;
@@ -111,5 +142,14 @@ void T2ISR(void)
     global_count++;
     //atomically clear the interrupt flag
     mT2ClearIntFlag();
+    while(1);
 }
 
+void T3ISR(void)
+{
+    //increment global counter
+    global_count_2++;
+    //atomically clear the interrupt flag
+    mT3ClearIntFlag();
+
+}
