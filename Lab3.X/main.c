a59592e329b2e2072e6de556d9bdcc835a86e07f
Cal <nahallacman@gmail.com>
2014-11-06 22:35:33 -0800

    Some more work on lab 3
    up to part 6 completed

--- Lab3.X/main.c
+++ Lab3.X/main.c
@@ -43,11 +43,12 @@
 #pragma config FSOSCEN=OFF, IESO=OFF
 
 
+//set the priority of the timer2 service routine
+#pragma interrupt T2ISR IPL4 vector 8
 
+void T2ISR(void);
 
-
-
-
+int global_count;
 
 //	Function Prototypes
 int main(void);
@@ -63,26 +64,52 @@ int main(void) {
     //configure timer 2
     PR2 = 511; //set the period register to interrupt every 512 counts
     T2CONbits.TCKPS = 0; //set the prescaler to 1:1
+    TMR2 = 0x0; // zero out the timer value just in case
 
     //turn on multi vectored mode
     INTCONbits.MVEC = 1;
 
-    //set the priority of the service routine
-    #pragma interrupt T2ISR IPL4 vector 8
-    
+    //configure the timer 2 priority
+    mT2SetIntPriority(4);
+    // and sub priority
+    mT2SetIntSubPriority(0);
+    //Then enable the interrupt (TODO: double check if this is an atomic operation)
+    mT2IntEnable(1);
+
     //enable interrupts
     asm("ei");
+    
+    //configure the LED for output
+    TRISDCLR = 1;
 
-    //T2CONbits.ON = 1; // start the timer
+    //initalize global counter
+    global_count = 0;
+
+    T2CONbits.ON = 1; // start the timer
 
 
     int a = 0;
-    while (1);
+    int MAX_NUM = 7200000;
+    while (1)
     {
-        a++;
-        a--;
+        if(a > MAX_NUM)
+        {
+            a = 0;
+            //TMR2 = 0x0; // reset the timer
+            PORTDINV = 1;
+        }
+        else
+        {
+            a++;
+        }
     }
 }
 
-
+void T2ISR(void)
+{
+    //increment global counter
+    global_count++;
+    //atomically clear the interrupt flag
+    mT2ClearIntFlag();
+}
 
