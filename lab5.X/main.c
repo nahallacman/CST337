a79ae26284d1a6beb29ae44cb59223f7430468d4
Cal <nahallacman@gmail.com>
2014-11-30 00:16:30 -0800

    Lab 5 part 1 more
    part 1 almost complete. Only thing missing is to poll the status
    register for a WIP (Write In Progress) on the LC256

--- lab5.X/main.c
+++ lab5.X/main.c
@@ -51,8 +51,12 @@ char SPI_SET_WRITE_ENABLE_LATCH = 0b00000110;
 
 char SPI_WRITE_ENABLE_MASK = 0b00000010;
 
-char DUMMY_DATA = 0x5F;
-
+int WRITE_LENGTH = 63; //0-63 = 64 divisions
+int READ_LENGTH = 63;
+char DUMMY_DATA = 0xE5;
+char TEST_DATA = 0x55;
+char ADDR_MSA = 0x10;//testing things out with a read to the very first page (or last page)
+char ADDR_LSA = 0x00;//lsb doesn't matter since we are doing reads on a page
 
 #pragma config POSCMOD=XT, FNOSC=PRIPLL, FPLLIDIV=DIV_2, FPLLMUL=MUL_20, FPLLODIV=DIV_1
 #pragma config FPBDIV=DIV_1, FWDTEN=OFF, CP=OFF, BWP=OFF
@@ -61,6 +65,20 @@ char DUMMY_DATA = 0x5F;
 int main(void) {
     SYSTEMConfig(80000000L, SYS_CFG_WAIT_STATES | SYS_CFG_PCACHE);
 
+    //variable initialization
+
+    int i = 0;
+    int status = 0x55;
+    int status2 = 0x55;
+    char iBuff[64];
+    char oBuff[64];
+
+    for(i = 0; i < 64; i++)
+    {
+        oBuff[i] = i;
+    }
+
+    oBuff[0] = 0xFF;
 //---Begin SPI configuration---
     //RB10 = CS (Chip Select), output, active low, initialize to 1 before setting to output
     
@@ -93,10 +111,7 @@ int main(void) {
     SPI1CON = 0x8220;
     */
 
-    int i = 0;
-    int status = 0x55;
-    int status2 = 0x55;
-    i =  SPI1STATbits.SPITBE;
+
 
 
 //---End SPI Configuration---
@@ -179,7 +194,7 @@ int main(void) {
     //1. Assert CS
     LATBbits.LATB10 = 0;
      //2. Write a read status command to the 25LC256 (write a read status command to SPI1BUF).
-    SPI1BUF = SPI_READ_STATUS_REGISTER;
+    SPI1BUF = SPI_WRITE_DATA_MEMORY;
     //SPI_READ_STATUS_REGISTER = SPI1BUF;
     //3. Wait for TBE (transmitter buffer empty)
     while( SPI1STATbits.SPITBE == 0 );
@@ -206,19 +221,130 @@ int main(void) {
     LATBbits.LATB10 = 1;
 
 //---End SECOND Read Status Command---
+    asm("nop");
+    asm("nop");
+    asm("nop");
+    asm("nop");
+//---Begin Page Write Command---
+//all writes wrap within their 64 byte page. This means the max write length is 64 bytes.
+    //1. Assert CS
+    LATBbits.LATB10 = 0;
+     //2. Write a read status command to the 25LC256 (write a read status command to SPI1BUF).
+    SPI1BUF = SPI_WRITE_DATA_MEMORY;
+    //SPI_READ_STATUS_REGISTER = SPI1BUF;
+    //3. Wait for TBE (transmitter buffer empty)
+    while( SPI1STATbits.SPITBE == 0 );
+    //4. Write the address MSB
+    SPI1BUF = ADDR_MSA;
+    //5. Wait for RBF (receive buffer full)
+    while(SPI1STATbits.SPIRBF == 0);
+    //6. Read SPI1BUF and discard the dummy data
+    SPI1BUF;
+    //7. Write LSA
+    SPI1BUF = ADDR_LSA;
+ 
+    //loop for data write
+    for(i = 0; i < WRITE_LENGTH; i++) //loop two less than the actual number of
+    {
+        //11. Wait for RBF
+        while(SPI1STATbits.SPIRBF == 0);
+        //12. discard dummy data
+        SPI1BUF;
+        //13.
+        SPI1BUF = oBuff[i]; // REAL DATA not actual dummy data
+    }
 
-    if(status == 2)
+    //14. Wait for RBF
+    while(SPI1STATbits.SPIRBF == 0);
+    //15. discard dummy data
+    SPI1BUF;
+    //16. Wait for RBF
+    while(SPI1STATbits.SPIRBF == 0);
+    //17. discard dummy data
+    SPI1BUF;
+
+    //18. Negate CS.
+    asm("nop");
+    asm("nop");
+    asm("nop");
+    asm("nop");
+    asm("nop");
+    asm("nop");
+    asm("nop");
+    asm("nop");
+    LATBbits.LATB10 = 1;
+//---End Page Write Command---
+    asm("nop");
+    asm("nop");
+    asm("nop");
+    asm("nop");
+ //---Begin Page Read Command---
+//all writes wrap within their 64 byte page. This means the read length is 64 bytes.
+    //1. Assert CS
+    LATBbits.LATB10 = 0;
+     //2. Write a read status command to the 25LC256 (write a read status command to SPI1BUF).
+    SPI1BUF = SPI_READ_DATA_MEMORY;
+    //SPI_READ_STATUS_REGISTER = SPI1BUF;
+    //3. Wait for TBE (transmitter buffer empty)
+    while( SPI1STATbits.SPITBE == 0 );
+    //4. Write the address MSB
+    SPI1BUF = ADDR_MSA;
+    //5. Wait for RBF (receive buffer full)
+    while(SPI1STATbits.SPIRBF == 0);
+    //6. Read SPI1BUF and discard the dummy data
+    SPI1BUF;
+    //7. Write LSA
+    SPI1BUF = ADDR_LSA;
+
+    
+    //8. Wait for RBF
+    while(SPI1STATbits.SPIRBF == 0);
+    //9. Read SPI1BUF and discard the dummy data
+    SPI1BUF;
+    //10. Write dummy data
+    SPI1BUF = DUMMY_DATA;
+    
+    
+    //8. Wait for RBF
+    while(SPI1STATbits.SPIRBF == 0);
+    //9. Read SPI1BUF and discard the dummy data
+    SPI1BUF;
+    //10. Write dummy data
+    SPI1BUF = DUMMY_DATA;
+    
+    
+    //loop for data write
+    for(i = 0; i < READ_LENGTH - 2; i++) //loop two less than the actual number of
     {
-        //no problems
+        //11. Wait for RBF
+        while(SPI1STATbits.SPIRBF == 0);
+        //12. save recieved byte
+        iBuff[i] = SPI1BUF;
+        //13. write dummy data
+        SPI1BUF = DUMMY_DATA; // 
     }
-    else
-    {
-        //problems
-    }
+
+    //14. Wait for RBF
+    while(SPI1STATbits.SPIRBF == 0);
+    //15. Read second to last byte of data
+    iBuff[62] = SPI1BUF;
+    //16. Wait for RBF
+    while(SPI1STATbits.SPIRBF == 0);
+    //17. Read last byte of data
+    iBuff[63] = SPI1BUF;
+
+    //18. Negate CS.
+    asm("nop");
+    asm("nop");
+    asm("nop");
+    asm("nop");
+    asm("nop");
+    asm("nop");
+    asm("nop");
+    asm("nop");
+    LATBbits.LATB10 = 1;
+//---End Page Write Command---
 
 
     while (1);
 }
-
-
-
