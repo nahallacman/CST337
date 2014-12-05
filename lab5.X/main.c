d4f74843b5823408b53e5d651621f93679d4ae63
Cal <nahallacman@gmail.com>
2014-12-05 02:44:21 -0800

    write not done
    Read's seem to be working fine, but the write ISR still needs work. It
    appears that there is a problem with CS not being asserted and negated
    properly. This may be due to the locations of asserts and negations but
    also may be due to an imbalance in how the SPI1BUF is read and written.

--- lab5.X/main.c
+++ lab5.X/main.c
@@ -62,13 +62,14 @@ char SPI_WRITE_ENABLE_MASK = 0b00000010;
 
 int WRITE_LENGTH = 63; //0-63 = 64 divisions
 int READ_LENGTH = 63;
-char DUMMY_DATA = 0xE5;
+char DUMMY_DATA = 0x45;
 char TEST_DATA = 0x55;
-char ADDR_MSA = 0x00;//testing things out with a read to the very first page (or last page)
+char ADDR_MSA = 0x20;//testing things out with a read to the very first page (or last page)
 char ADDR_LSA = 0x00;//lsb doesn't matter since we are doing reads on a page
 
 int entrycount = 0;
 
+//ISR variables
 int EEPromSysBusy = 0; // initalize to 0, will tell the user if the system is busy using the SPI device or not
 unsigned int state = 0; // controls the state machine
 int Rnbytes = 0; // use for global access of how many bytes to read
@@ -76,8 +77,18 @@ char RADDR_MSA = 0x00;
 char RADDR_LSA = 0x00;
 char * RBUFF;
 unsigned int READNUM = 0; // internal ISR counter for number of bytes to read still
+char rstatus = 0;
+
+int Wnbytes = 0;
+char WADDR_MSA = 0x00;
+char WADDR_LSA = 0x00;
+char * WBUFF;
+unsigned int WRITENUM = 0;
 
 #define CHECKSTATUS 0
+#define CHECKSTATUSREAD1 200
+#define CHECKSTATUSREAD2 201
+#define CHECKSTATUSREAD3 202
 #define READ1 1
 #define READ2 2
 #define READ3 3
@@ -90,7 +101,19 @@ unsigned int READNUM = 0; // internal ISR counter for number of bytes to read st
 #define READ10 10
 #define READ11 11
 #define N1 100
+#define CHECKWELSTATUS1 300
+#define CHECKWELSTATUS2 301
+#define CHECKWELSTATUS3 302
+#define SETWEL1 303
+#define SETWEL2 304
+#define CHECKSTATUSWRITE1 400
+#define CHECKSTATUSWRITE2 401
+#define CHECKSTATUSWRITE3 402
 #define WRITE1 1000
+#define WRITE2 1001
+#define WRITE3 1002
+#define WRITE4 1003
+#define WRITE5 1004
 
 #pragma config POSCMOD=XT, FNOSC=PRIPLL, FPLLIDIV=DIV_2, FPLLMUL=MUL_20, FPLLODIV=DIV_1
 #pragma config FPBDIV=DIV_1, FWDTEN=OFF, CP=OFF, BWP=OFF
@@ -105,11 +128,13 @@ int main(void) {
     int status = 0x55;
     int status2 = 0x55;
     char iBuff[64];
+    char iBuff2[64];
     char oBuff[64];
 
     for(i = 0; i < 64; i++)
     {
         iBuff[i] = 0;
+        iBuff2[i] = 0;
         oBuff[i] = i;
     }
 
@@ -191,7 +216,7 @@ int main(void) {
 
 
 
- 
+
 //---Begin SPI Read Status command--- //2 byte shifted out
     //communication pattern for read status
     //1. Assert CS.
@@ -270,7 +295,7 @@ int main(void) {
     //1. Assert CS
     LATBbits.LATB10 = 0;
      //2. Write a read status command to the 25LC256 (write a read status command to SPI1BUF).
-    SPI1BUF = SPI_WRITE_DATA_MEMORY;
+    SPI1BUF = SPI_READ_STATUS_REGISTER;
     //SPI_READ_STATUS_REGISTER = SPI1BUF;
     //3. Wait for TBE (transmitter buffer empty)
     while( SPI1STATbits.SPITBE == 0 );
@@ -301,7 +326,8 @@ int main(void) {
     asm("nop");
     asm("nop");
     asm("nop");
- 
+
+
 //---Begin Page Write Command---
 //all writes wrap within their 64 byte page. This means the max write length is 64 bytes.
     //1. Assert CS
@@ -398,8 +424,8 @@ while(status & 0b00000001);// check if work in progress bit is set
     asm("nop");
     asm("nop");
 
-    
- //---Begin Page Read Command---
+    /*
+//---Begin Page Read Command---
 //all writes wrap within their 64 byte page. This means the read length is 64 bytes.
     //1. Assert CS
     LATBbits.LATB10 = 0;
@@ -471,8 +497,14 @@ while(status & 0b00000001);// check if work in progress bit is set
     {
         iBuff[i] = 0;
     }
+*/
 
-    ReadEEProm(64, 0x0000, iBuff);
+    ReadEEProm(64, 0x2000, iBuff);
+
+    WriteEEProm(64, 0x4000, oBuff);
+
+    //ReadEEProm(64, 0x4000, iBuff2);
+    
     while (1);
 }
 
@@ -483,29 +515,29 @@ void ReadEEProm(int nbytes, unsigned int address, unsigned char* readbuffer)
 {
     if( nbytes != 0)
     {
-    //check EEPromSysBusy until it is 0
-    while(EEPromSysBusy);
+        //check EEPromSysBusy until it is 0
+        while(EEPromSysBusy);
 
-    asm("di"); //disable interrupts while global variables are being accessed
-    //prepare the system to read nbytes bytes.
+        asm("di"); //disable interrupts while global variables are being accessed
+        //prepare the system to read nbytes bytes.
 
-    state = READ1;
-    EEPromSysBusy = 1;
-    Rnbytes = nbytes;
-    RADDR_MSA = (address >> 8);
-    RADDR_LSA = address & 0x00FF;
-    RBUFF = readbuffer; // this may need to copy address of instead
-    READNUM = 0; // initalize to 0
-    
-    asm("ei"); //enable interrupts after global variables are being accessed
-    
-    //manually set the SPI1 TX interrupt flag
-    //IFS0SET = 0x01000000;
-    //manually set the SPI1 RX interrupt flag
-    IFS0SET = 0x02000000;
-    //manually set the SPI1 error interrupt flag
-    //IFS0SET = 0x04000000;
-    //IFS0bits.SPI1TXIF = 1;
+        state = CHECKSTATUSREAD1;
+        EEPromSysBusy = 1;
+        Rnbytes = nbytes;
+        RADDR_MSA = (address >> 8);
+        RADDR_LSA = address & 0x00FF;
+        RBUFF = readbuffer; // this may need to copy address of instead
+        READNUM = 0; // initalize to 0
+
+        asm("ei"); //enable interrupts after global variables are being accessed
+
+        //manually set the SPI1 TX interrupt flag
+        //IFS0SET = 0x01000000;
+        //manually set the SPI1 RX interrupt flag
+        IFS0SET = 0x02000000;
+        //manually set the SPI1 error interrupt flag
+        //IFS0SET = 0x04000000;
+        //IFS0bits.SPI1TXIF = 1;
 
     }
     else
@@ -518,13 +550,32 @@ void ReadEEProm(int nbytes, unsigned int address, unsigned char* readbuffer)
 
 void WriteEEProm(int nbytes, unsigned int address, unsigned char* writebuffer)
 {
-    //check EEPromSysBusy until it is 0
-    while(EEPromSysBusy);
-    //prepare the system to read nbytes bytes.
-    state = WRITE1;
+    if( nbytes != 0)
+    {
+        //check EEPromSysBusy until it is 0
+        while(EEPromSysBusy);
 
+        asm("di"); //disable interrupts while global variables are being accessed
+        //prepare the system to read nbytes bytes.
 
+        state = CHECKWELSTATUS1;
+        EEPromSysBusy = 1;
+        Wnbytes = nbytes;
+        WADDR_MSA = (address >> 8);
+        WADDR_LSA = address & 0x00FF;
+        WBUFF = writebuffer;
+        WRITENUM = 0; // re-initalize to 0
 
+        asm("ei"); //enable interrupts after global variables are being accessed
+
+        //manually set the SPI1 RX interrupt flag
+        IFS0SET = 0x02000000;
+
+    }
+    else
+    {
+        //error?
+    }
     return;
 }
 
@@ -539,6 +590,61 @@ void SPI1ISR()
         case CHECKSTATUS:
             entrycount++;
             state = CHECKSTATUS;
+            break;
+
+        case CHECKSTATUSREAD1: //--- Begin Check status register until write is done---
+
+            //1. Assert CS
+            LATBbits.LATB10 = 0;
+             //2. Write a read status command to the 25LC256 (write a read status command to SPI1BUF).
+            SPI1BUF = SPI_READ_STATUS_REGISTER;
+            //SPI_READ_STATUS_REGISTER = SPI1BUF;
+            //3. Wait for TBE (transmitter buffer empty)
+            while( SPI1STATbits.SPITBE == 0 );
+            //4. Write a dummy data byte to SPI1BUF (we need to write a byte to get the SPI to clock the returned status byte in)
+            SPI1BUF = DUMMY_DATA;
+            state = CHECKSTATUSREAD2;
+            break;
+
+        case CHECKSTATUSREAD2:
+            //5. Wait for RBF (receive buffer full) which will be set after the read status command is fully shifted out.
+            //while(SPI1STATbits.SPIRBF == 0);
+            //6. Read SPI1BUF and discard the dummy data that was clocked in while the read status command was sent out.
+            SPI1BUF;
+
+            state = CHECKSTATUSREAD3;
+            break;
+
+        case CHECKSTATUSREAD3:
+            //7. Wait for RBF which will be set after the dummy data byte (sent at step 3) is clocked out
+            //while(SPI1STATbits.SPIRBF == 0);
+            //8. Read the status byte which was clocked in from the 25LC256 while the dummy data byte (sent at step 3) was clocked out.
+            rstatus = SPI1BUF;
+
+            /*
+            //9. Negate CS.
+            asm("nop");
+            asm("nop");
+            asm("nop");
+            asm("nop");
+            asm("nop");
+            asm("nop");
+            asm("nop");
+            asm("nop");
+            */
+            LATBbits.LATB10 = 1;
+
+            // check if work in progress bit is set
+            if(rstatus & 0b00000001)
+            {
+                state = CHECKSTATUSREAD1; //if it is set, keep checking until it isn't
+            }
+            else
+            {
+                state = READ1;
+            }
+            //manually set the SPI1 RX interrupt flag so the ISR will re-enter
+            IFS0SET = 0x02000000;
             break;
 
         case READ1:
@@ -594,9 +700,6 @@ void SPI1ISR()
         case READ4: //11. Wait for RBF
 
             if( READNUM < Rnbytes - 3 )
-            //if( READNUM < Rnbytes - 4 )
-            //if( READNUM < Rnbytes - 2)
-            //if( READNUM >= Rnbytes - 3)
             {}
             else
             {
@@ -654,8 +757,149 @@ void SPI1ISR()
             //---End Page Write Command---
             state = CHECKSTATUS;
             break;
-        case WRITE1:
+
+        case CHECKWELSTATUS1:
+            //1. Assert CS
+            LATBbits.LATB10 = 0;
+             //2. Write a read status command to the 25LC256 (write a read status command to SPI1BUF).
+            SPI1BUF = SPI_READ_STATUS_REGISTER;
+            //SPI_READ_STATUS_REGISTER = SPI1BUF;
+            //3. Wait for TBE (transmitter buffer empty)
+            while( SPI1STATbits.SPITBE == 0 );
+            //4. Write a dummy data byte to SPI1BUF (we need to write a byte to get the SPI to clock the returned status byte in)
+            SPI1BUF = DUMMY_DATA;
+            state = CHECKWELSTATUS2;
             break;
+
+        case CHECKWELSTATUS2:
+            //5. Wait for RBF (receive buffer full) which will be set after the read status command is fully shifted out.
+            //while(SPI1STATbits.SPIRBF == 0);
+            //6. Read SPI1BUF and discard the dummy data that was clocked in while the read status command was sent out.
+            SPI1BUF;
+            state = CHECKWELSTATUS3;
+            break;
+
+        case CHECKWELSTATUS3:
+            //7. Wait for RBF which will be set after the dummy data byte (sent at step 3) is clocked out
+            // while(SPI1STATbits.SPIRBF == 0);
+            //8. Read the status byte which was clocked in from the 25LC256 while the dummy data byte (sent at step 3) was clocked out.
+            rstatus = SPI1BUF;
+            //9. Negate CS.
+            /*
+            asm("nop");
+            asm("nop");
+            asm("nop");
+            asm("nop");
+            asm("nop");
+            asm("nop");
+            asm("nop");
+            asm("nop");
+            LATBbits.LATB10 = 1;
+            */
+
+            //check if write enable latch is not set
+            if((rstatus & 0b00000010) == 0)
+            {
+                state = SETWEL1;
+            }
+            // check if write in progress bit is set
+            else if(rstatus & 0b00000001)
+            {
+                state = CHECKSTATUSWRITE1; //if it is set, keep checking until it isn't
+            }
+            else
+            {
+                state = WRITE1;
+            }
+            //manually set the SPI1 RX interrupt flag so the ISR will re-enter
+            IFS0SET = 0x02000000;
+            LATBbits.LATB10 = 1;
+            break;
+
+        case SETWEL1:
+            //---Begin Set Write Enable Latch Command--- // 1 byte shifted out
+           //note: this may not need the second read/write cycle
+             //1. Assert CS
+            LATBbits.LATB10 = 0;
+            //2. Write a write status command to the 25LC256
+            SPI1BUF = SPI_SET_WRITE_ENABLE_LATCH;
+            state = SETWEL2;
+            break;
+
+        case SETWEL2:
+            //3. Wait for RBF (receive buffer full) which will be set after the write status command is fully shifted out.
+            //4. Read dummy data
+            SPI1BUF;
+            //5. Negate CS.
+            asm("nop");
+            asm("nop");
+            asm("nop");
+            asm("nop");
+            asm("nop");
+            asm("nop");
+            asm("nop");
+            asm("nop");
+            LATBbits.LATB10 = 1;
+
+            state = CHECKWELSTATUS1;
+            //manually set the SPI1 RX interrupt flag so the ISR will re-enter
+            IFS0SET = 0x02000000;
+            break;
+
+        case WRITE1:
+            //1. Assert CS
+            LATBbits.LATB10 = 0;
+             //2. Write a read status command to the 25LC256 (write a read status command to SPI1BUF).
+            SPI1BUF = SPI_WRITE_DATA_MEMORY;
+            //SPI_READ_STATUS_REGISTER = SPI1BUF;
+            //3. Wait for TBE (transmitter buffer empty)
+            while( SPI1STATbits.SPITBE == 0 );
+            //4. Write the address MSB
+            SPI1BUF = WADDR_MSA;
+            state = WRITE2;
+            break;
+
+        case WRITE2:
+            //5. Wait for RBF (receive buffer full)
+            //while(SPI1STATbits.SPIRBF == 0);
+            //6. Read SPI1BUF and discard the dummy data
+            SPI1BUF;
+            //7. Write LSA
+            SPI1BUF = WADDR_LSA;
+            state = WRITE3;
+            break;
+          
+        case WRITE3: //11. Wait for RBF
+            if( WRITENUM + 2 > Wnbytes  )
+            {
+                state = WRITE4;
+            }
+            //12. discard dummy data
+            SPI1BUF;
+            //13.
+            SPI1BUF = WBUFF[WRITENUM]; // REAL DATA not actual dummy data
+            WRITENUM++;
+            break;
+            
+        case WRITE4:
+            //14. Wait for RBF
+            //while(SPI1STATbits.SPIRBF == 0);
+            //15. discard dummy data
+            SPI1BUF;
+            state = WRITE5;
+            break;
+
+        case WRITE5:
+            //16. Wait for RBF
+            //while(SPI1STATbits.SPIRBF == 0);
+            //17. discard dummy data
+            SPI1BUF;
+
+            //18. Negate CS.
+            LATBbits.LATB10 = 1;
+            state = CHECKSTATUS; // what should actually go here?
+            break;
+            
         default:
             //error?
             break;
