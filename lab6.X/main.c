d177fbff168cb9108fdd3e19c8a94ac27145fc19
Cal <nahallacman@gmail.com>
2014-12-10 02:54:49 -0800

    Lab 6 part 1 complete
    Hardware controlled TX, RX, CTS, and RTS, software control repeats a
    sent character, new characters change the sent character, but keeps
    repeating.

--- lab6.X/main.c
+++ lab6.X/main.c
@@ -48,6 +48,9 @@ int main(void);
 #pragma config FPBDIV=DIV_1, FWDTEN=OFF, CP=OFF, BWP=OFF
 #pragma config FSOSCEN=OFF, IESO=OFF
 
+char const TEST_DATA = 0x55;
+char recieved = 0x55; // for testing
+
 int main(void) {
     //set the system to 80MHz operation, auto(2) wait states, use prefetch cache, use instruction cache, disable bmx breakpoints
     SYSTEMConfig(80000000L, SYS_CFG_WAIT_STATES | SYS_CFG_PCACHE);
@@ -81,24 +84,32 @@ int main(void) {
     //---end button config---
 
     //---begin UART1 config---
+
+
+
     U1MODEbits.BRGH = 0;// standard 16x mode
-    U1MODEbits.UEN = 0b10; //CTS and RTS are under software control
+    U1MODEbits.UEN = 0b10; //CTS and RTS are under hardware control
     U1MODEbits.PDSEL = 0b00; //8 bits no parity
     U1MODEbits.RTSMD = 0; //UxRTS pin is in Flow Control mode
 
     //U1MODEbits.UEN = 0b11; // use this mode to test the baud clock on pin RD16
+    //U1MODEbits.UEN = 0b00; // guess testing
 
     U1STA = 0; //clear status register just in case
+
+    U1STAbits.URXEN = 1; //enable hardware control of RX pin
+    U1STAbits.UTXEN = 1; //enable hardware control of TX pin
+    U1STAbits.UTXISEL = 0b10; // interrupt when the transmit buffer becomes empty
 
     //intended baud rate = 115.2k baud
     //Baud Rate = Fpb / (16 * (U1BRG + 1))
     //or U1BRG = Fpb / ( 16 * Baud Rate ) - 1
     // ( 80000000 / ( 16 * 115200 ) )  - 1 = 42.402778 or about 42
-    //U1BRG = 42;
+    U1BRG = 42;
 
     //intended baud rate = 9600 baud
     // ( 80000000 / ( 16 * 9600 ) )  - 1 = 519.83333333333333333333333333333 or about 520
-    U1BRG = 520;
+    //U1BRG = 520; // THIS VALUE BREAKS THINGS -> possiblites? 512 max value?
 
     //U1MODEbits.LPBACK = 1; // turn on loopback for testing purposes
 
@@ -129,7 +140,7 @@ int main(void) {
 
 void UART1ISR()
 {
-    char recieved = 0; // for testing
+    //char recieved = 0; // for testing
     
     //needs to check if the transmit or recieve interrupt flag is set?
     //IFS0CLR = 0x1C000000;  //bits 26(error), 27(RX), and 28(TX)
@@ -141,12 +152,18 @@ void UART1ISR()
     }
     else if(IFS0bits.U1RXIF == 1)
     {
+        int temp = 0;
         //start RX LED timer
         //turn on RX LED
         PORTDbits.RD1 = 1;
         IFS0CLR = 0x8000000; // clear recieve flag
         //read from U1 RX buffer
-        recieved = U1RXREG;
+        temp = U1RXREG;
+        if(temp)
+        {
+            recieved = temp;
+        }
+        U1TXREG = recieved;
     }
     else if(IFS0bits.U1TXIF == 1)
     {
