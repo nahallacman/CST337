3079f315556aab48e06e02556707ec47cf5d90dd
Cal <nahallacman@gmail.com>
2014-12-13 02:59:58 -0800

    Lab 6 completed

--- lab6.X/main.c
+++ lab6.X/main.c
@@ -1,6 +1,6 @@
 /*
  * File:   main.c
- * Author: cal
+ * Author: Cal Barkman
  *
  * Created on December 5, 2014, 11:10 PM
  */
@@ -34,17 +34,23 @@
 
 //set the priority of the UART1 service routine
 #pragma interrupt UART1ISR IPL7 vector 24
+//set the priority of the Timer 1 service routine
+#pragma interrupt T1ISR IPL1 vector 4
 //set the priortiy of the Timer 2&3 service routine
-#pragma interrupt T23ISR IPL2 vector 12
+#pragma interrupt T23ISR IPL3 vector 12
 //set the priortiy of the Timer 4&5 service routine
-#pragma interrupt T45ISR IPL1 vector 20
+#pragma interrupt T45ISR IPL2 vector 20
 
-//UART1 ISR declaration
+
+//UART1 ISR definition
 void UART1ISR();
+//timer 1 ISR declaration
+void T1ISR();
 //Timer2and3 ISR declaration
 void T23ISR();
 //timer 4and5 ISR declaration
 void T45ISR();
+
 
 #include <p32xxxx.h>
 #include <plib.h>
@@ -58,6 +64,22 @@ int main(void);
 
 char const TEST_DATA = 0x55;
 char recieved = 0x55; // for testing
+
+//variables for circular buffer
+char buffer[1000]; //8KB buffer, needs to go into RAM
+char * h;
+char * t;
+char * begin;
+char * end;
+int num_items;
+int max = 1000;
+
+
+
+int T1STATE = 0;
+int debouncestate = 0;
+int buttonpressed = 0;
+
 
 int main(void) {
     //set the system to 80MHz operation, auto(2) wait states, use prefetch cache, use instruction cache, disable bmx breakpoints
@@ -89,9 +111,9 @@ int main(void) {
     PR2 = 40000000; // 1/2 second period
 
     IFS0bits.T3IF = 0; // clear flag
-    IEC0bits.T3IE = 1; // enable
+    IEC0bits.T3IE = 1; // enable interrupt
 
-    IPC3bits.T3IP = 0b010; // priority 2
+    IPC3bits.T3IP = 0b011; // priority 3
     IPC3bits.T3IS = 0; // subpriority 0
 
     //using timers 4 and 5 for 1/2 a second delay of TX LED
@@ -103,23 +125,32 @@ int main(void) {
     PR4 = 40000000; // 1/2 second period
 
     IFS0bits.T5IF = 0; // clear flag
-    IEC0bits.T5IE = 1; // enable
+    IEC0bits.T5IE = 1; // enable interrupt
 
-    IPC5bits.T5IP = 0b001; // priority 1
+    IPC5bits.T5IP = 0b010; // priority 2
     IPC5bits.T5IS = 0; // subpriority 0
 
     //---end LED config---
 
     //---begin button config---
     //using button RC1
+    TRISCbits.TRISC1 = 1; // set to input
+    
 
     //using timer 1 for 15ms debounce checks
+    TMR1 = 0; //clear the timer just in case
+    T1CONbits.TCKPS = 0b11; //1:256 prescaler
+    PR1 = 46875; //15ms delay, (80,000,000 / 256) * .15 = 46875, max PR1 val is 65535
 
+    IFS0bits.T1IF = 0; // clear flag
+    IEC0bits.T1IE = 1; // enable interrupt
+
+    IPC1bits.T1IP = 0b001; // priority 1
+    IPC1bits.T1IS = 0; // subpriority 0
     //---end button config---
 
+
     //---begin UART1 config---
-
-
 
     U1MODEbits.BRGH = 0;// standard 16x mode
     U1MODEbits.UEN = 0b10; //CTS and RTS are under hardware control
@@ -148,7 +179,6 @@ int main(void) {
     //U1MODEbits.LPBACK = 1; // turn on loopback for testing purposes
 
     U1MODEbits.ON = 1; // turn UART1 on at the end of config
-    //---end UART1 config---
 
     //---begin Interrupt config---
     IFS0bits.U1RXIF = 0; //clear interrupt flags just in case
@@ -163,22 +193,45 @@ int main(void) {
     IEC0bits.U1EIE = 1;
     //---end Interrupt config---
 
-    asm("ei");
-    T2CONbits.ON = 1; //start timer 2 for testing
-    //turn on RX LED for testing
-    PORTDbits.RD1 = 1;
+    //---begin circular buffer init
+    num_items = 0;
+    begin = h = t = buffer;
+    //buffer;
+    end = begin + max;
+    // I think I may be off by one
+    //end = begin + max - 1;
+    //circular buffer LED config
+    TRISBbits.TRISB4 = 0;//using LED RB4 (output)
+    PORTBbits.RB4 = 1;//init to on
+    //---end circular buffer init
 
-    T4CONbits.ON = 1; //start timer 4 for testing
-    //turn on TX LED for testing
-    PORTDbits.RD2 = 1;
+    //---end UART1 config---
 
-    //IFS0bits.U1RXIF = 1; //manually set flag to test interrupts
-    //IFS0bits.U1TXIF = 1;
-    //IFS0bits.U1EIF = 1;
 
-    //IFS0bits.T3IF = 1;
 
-    while (1);
+     asm("ei");
+
+    int defaultstate = 0;
+
+    while (1)
+    {
+        if(buttonpressed == 1)
+        {
+            if(PORTCbits.RC1 == 1)
+            {
+                T1CONbits.ON = 1;
+                //IFS0bits.T1IF = 1; // set timer flag to start button press state machine
+            }
+        }
+        else
+        {
+            if(PORTCbits.RC1 == 0)
+            {
+                T1CONbits.ON = 1;
+                //IFS0bits.T1IF = 1; // set timer flag to start button press state machine
+            }
+        }
+    }
 }
 
 void UART1ISR()
@@ -200,13 +253,38 @@ void UART1ISR()
         //turn on RX LED
         PORTDbits.RD1 = 1;
         IFS0CLR = 0x8000000; // clear recieve flag
+
         //read from U1 RX buffer
+        /*
         temp = U1RXREG;
         if(temp)
         {
             recieved = temp;
         }
         U1TXREG = recieved;
+         */
+
+        // do we have to check if the value is a break first?
+
+        //begin circular buffer insert
+        if(num_items < max) // then the buffer isn't full
+        {
+            *h = U1RXREG;
+            num_items++;
+            h++;
+            if( h > end )
+            {
+                h = begin;
+            }
+        }
+        else
+        {
+            //turn on buffer error LED
+            PORTBbits.RB4 = 0;
+            //do I try to keep reading here?
+            U1RXREG;
+        }
+
         TMR2 = 0; //clear the timer
         T2CONbits.ON = 1; // start the timer
     }
@@ -216,14 +294,28 @@ void UART1ISR()
         //turn on TX LED
         PORTDbits.RD2 = 1;
         IFS0CLR = 0x10000000; // clear transmit flag
+
         //write more to the U1 TX buffer
-        U1TXREG = recieved;
+        //U1TXREG = recieved;
+        
+        //begin circular buffer removal
+        if(num_items > 0)
+        {
+            U1TXREG = *t;
+            num_items--;
+            t++;
+            if(t > end) // make buffer circular
+            {
+                t = begin;
+            }
+        }
+
         TMR4 = 0; //clear the timer
         T4CONbits.ON = 1; // start the timer
     }
 }
 
-//Timer2and3 ISR declaration
+//Timer2and3 ISR definition
 void T23ISR()
 {
     //clear T3IF flag atomically
@@ -233,7 +325,7 @@ void T23ISR()
     T2CONbits.ON = 0; // stop the timer
     TMR2 = 0; //clear the timer
 }
-//timer 4and5 ISR declaration
+//timer 4and5 ISR definition
 void T45ISR()
 {
     //clear T5IF flag atomically
@@ -243,4 +335,71 @@ void T45ISR()
     PORTDbits.RD2 = 0;
     T4CONbits.ON = 0; // stop the timer
     TMR4 = 0; //clear the timer
+}
+
+//timer 1 ISR definition
+void T1ISR()
+{
+    //clear T1IF flag atomically
+    IFS0CLR = 0x10;
+    if(buttonpressed == 1) //button is going from active to inactive (0 to 1)
+    {
+        switch(T1STATE)
+        {
+            case 0:
+                if(PORTCbits.RC1 == 1) // button still not pressed
+                {
+                    T1STATE = 1; // continue
+                }
+                else    //start the sequence over
+                {
+                    T1STATE = 0;
+                    T1CONbits.ON = 0;
+                    TMR1 = 0;
+                }
+                break;
+            case 1:
+                if(PORTCbits.RC1 == 1) // button still pressed
+                {
+                    buttonpressed = 0;
+                }
+                T1STATE = 0; // reset
+                T1CONbits.ON = 0;
+                TMR1 = 0;
+                break;
+            default:
+                break;
+        }
+    }
+    else // button is going from inactive to active ( 1 to 0 )
+    {
+        switch(T1STATE)
+        {
+            case 0:
+                if(PORTCbits.RC1 == 0) // button still pressed
+                {
+                    T1STATE = 1; // continue
+                }
+                else    //start the sequence over
+                {
+                    T1STATE = 0;
+                    T1CONbits.ON = 0;
+                    TMR1 = 0;
+                }
+                break;
+            case 1:
+                if(PORTCbits.RC1 == 0) // button still pressed
+                {
+                    buttonpressed = 1; // button press confirmed
+                    PORTBbits.RB4 = 1; // clear the overrun LED
+                    IFS0SET = 0x10000000; // set transmit flag to begin chain of transmits
+                }
+                T1STATE = 0; // reset
+                T1CONbits.ON = 0;
+                TMR1 = 0;
+                break;
+            default:
+                break;
+        }
+    }
 }
